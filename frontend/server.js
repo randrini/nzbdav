844b095a202fc76b348721c738e02ccc93472a4f
nzbdav-dev <nzbdavdev@protonmail.com>
2025-08-06 07:27:08 -0700

    Do not log successful frontend requests.

--- frontend/server.js
+++ frontend/server.js
@@ -37,7 +37,7 @@ if (DEVELOPMENT) {
     "/assets",
     express.static("build/client/assets", { immutable: true, maxAge: "1y" }),
   );
-  app.use(morgan("tiny"));
+  app.use(morgan("tiny", { skip: (_req, res) => res.statusCode < 400 }));
   app.use(express.static("build/client", { maxAge: "1h" }));
   app.use(await import(BUILD_PATH).then((mod) => mod.app));
 }
